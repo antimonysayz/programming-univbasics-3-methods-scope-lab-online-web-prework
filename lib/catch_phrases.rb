27c4da52983bcc86f7ef79bec587944dfbc2816e
Anthony Mai <anthony.mai530@gmail.com>
2019-10-20 19:27:39 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -5,6 +5,6 @@ def mario
 end
 
 def toadstool
-  status = "Thank You Mario! But our Princeess Is In Another Castle!"
+  status = "Thank You Mario! But Our Princeess Is In Another Castle!"
   puts status
 end
